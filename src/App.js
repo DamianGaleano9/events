c2f844cc0f85a349a6a1c90b1e096eb259f41a4f
Damian Galeano <dj.damian.b@hotmail.com>
2024-04-08 23:22:54 +0200

    UseRef

--- src/App.js
+++ src/App.js
@@ -3,42 +3,49 @@ import img0 from './images/0418d7df-78bb-4aa8-a14a-fd83412be197.jpg';
 import img1 from './images/marcocarola.jpg'
 import img2 from './images/capriaty.jpg'
 import img3 from './images/jamie.jpg'
+import { useRef } from 'react';
 
 function App() {
-  function incrementar(e) {
+  const cambio = 23.99;
+
+
+  const refCaja = useRef();
+
+
+    function incrementar(e) {
     e.target.innerHTML = Number(e.target.innerHTML) + 1;
     // e.target.style.backgroundColor= "black "
   }
 
 
 function convert() {
-
+  refCaja.current.innerHTML = Number(refCaja.current.innerHTML) * cambio
 }
 
   return (
     <>
       <div className='container'>
-        <div className='caja' onClick={incrementar}>1</div>
+        <div ref={refCaja}className='caja' onClick={incrementar}>1</div>
           <button onClick={convert}>accept</button>
           <div>
             <img src={img0} alt='' />
           </div>
           <input className='field'/>
 
-          <div className='caja' onClick={incrementar}>2</div >
+          <div ref={refCaja}className='caja' onClick={incrementar}>2</div >
             <button onClick={convert}>accept</button>
             <img src={img1} alt='' />
 
 
 
-            <div className='caja' onClick={incrementar}>3</div>
+            <div ref={refCaja}className='caja' onClick={incrementar}>3</div>
             <button onClick={convert}>accept</button>
             <div>
               <img src={img2} alt='' />
             </div>
             <input className='field'/>
 
-          <div className='caja' onClick={incrementar}>4</div>
+          <div ref={refCaja}className='caja' onClick={incrementar}>4</div>
           <button onClick={convert}>accept</button>
           <div>
             <img src={img3} alt='' />
